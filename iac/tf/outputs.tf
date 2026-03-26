eb3410b10439da0b12c8030c0f055087036827e3
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-03-26 17:29:02 +0900

    Update outputs.tf

--- iac/tf/outputs.tf
+++ iac/tf/outputs.tf
@@ -1,3 +1,15 @@
 output "vscode" {
   value = "http://${aws_instance.gpu_box.public_ip}:8080"
 }
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.main.name
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
+output "karpenter_role_arn" {
+  value = aws_iam_role.karpenter_controller.arn
+}
